c1c5d45b07822920a1ea623732a5c52e5b7c176b
Hamza Farooq <hamzafarooq109@gmail.com>
2026-02-20 02:00:50 +0500

    setup type module in package.json and setup dotenv

--- server.js
+++ server.js
@@ -1,7 +1,13 @@
 import express from "express"
 import morgan from "morgan"
+import dotenv from "dotenv"
+dotenv.config()
+
+const PORT=process.env.PORT||5000;
+console.log(PORT);
 
 const app=express();
+
 app.listen(PORT||5000,()=>{
     console.log(`server is running at http://localhost:${PORT}`);
 });
