f43b674f4274ba3de5330fae56f85bfdd0f673c7
Hamza Farooq <hamzafarooq109@gmail.com>
2026-02-23 12:59:11 +0500

    setup mongodb and cors in server.js

--- server.js
+++ server.js
@@ -2,19 +2,22 @@ import express from "express"
 import morgan from "morgan"
 import dotenv from "dotenv"
 import routes from "./Routes/route.js"
+import { dbConnect } from "./config/db.js"
+import cors from "cors"
 dotenv.config()
 
 const PORT=process.env.PORT||5000;
-console.log(PORT);
-
 const app=express();
 
-
-app.use(morgan('dev'));
+dbConnect();
+app.use(express.urlencoded({extended:true}));
 app.use(express.json());
+app.use(cors());
+app.use(morgan('dev'));
 
 app.use("/api",routes);
 
 app.listen(PORT||5000,()=>{
     console.log(`server is running at http://localhost:${PORT}`);
 });
+
